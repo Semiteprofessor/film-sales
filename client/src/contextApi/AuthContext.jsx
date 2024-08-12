df2cd3957e55c6699a2c7a92e111cec0396b5d8f
Taiwo Olapade <semiteprofessor@gmail.com>
2024-08-12 19:32:54 +0100

    Commit

--- client/src/contextApi/AuthContext.jsx
+++ client/src/contextApi/AuthContext.jsx
@@ -1,10 +1,4 @@
-import {
-  useReducer,
-  createContext,
-  useContext,
-  useState,
-  useCallback,
-} from "react";
+import { useReducer, createContext, useContext, useState } from "react";
 import axios from "axios";
 import { BASE_URL } from "../base";
 
@@ -59,62 +53,31 @@ const reducer = (state, action) => {
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-  // const [{ user, isAuthenticated, isLoading, error }, dispatch] = useReducer(
-  //   reducer,
-  //   initialState
-  // );
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [user, setUser] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState("");
+  const [{ user, isAuthenticated, isLoading, error }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
   const [paymentReference, setPaymentReference] = useState("");
 
-  // const loginUser = async (email, password) => {
-  //   await axios(`${BASE_URL}/user/login`, {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //     data: JSON.stringify({ email, password }),
-  //   }).then((response) => {
-  //     console.log(response.data);
-  //     if (response.status !== true) {
-  //       dispatch({
-  //         type: "LOGIN_FAILURE",
-  //         payload: { error: response.data.message },
-  //       });
-  //       return;
-  //     }
-  //     dispatch({ type: "LOGIN_SUCCESS", payload: { user: response.data } });
-  //   });
-  // };
-
-  const loginUser = useCallback(() => {
-    return async (email, password) => {
-      setIsLoading(true);
-      await axios(`${BASE_URL}/user/login`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        data: JSON.stringify({ email, password }),
-      })
-        .then((response) => {
-          console.log(response.data);
-          if (response.status !== 200) {
-            throw new Error(response.data.message);
-          }
-          localStorage.setItem("userToken", response.data.token);
-          setUser(response.data.data);
-          setIsAuthenticated(true);
-          setIsLoading(false);
-        })
-        .catch((error) => {
-          setError(error);
-          setIsLoading(false);
+  const loginUser = async (email, password) => {
+    await axios(`${BASE_URL}/user/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: JSON.stringify({ email, password }),
+    }).then((response) => {
+      console.log(response.data);
+      if (response.status !== true) {
+        dispatch({
+          type: "LOGIN_FAILURE",
+          payload: { error: response.data.message },
         });
-    };
-  }, []);
+        return;
+      }
+      dispatch({ type: "LOGIN_SUCCESS", payload: { user: response.data } });
+    });
+  };
 
   const registerUser = async (name, email, password, dob, phone, address) => {
     await axios(`${BASE_URL}/user/register`, {
